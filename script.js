9d47970fb949cf2edef0cdf61bb8688028f0bc1b
GoodCoder09878 <62258613+GoodCoder09878@users.noreply.github.com>
2020-07-26 16:27:03 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// functions
+function function0() {
+    alert("The book is not on stock now.");
+}
+function aboutus() {
+    alert("We are a non-profit website organisation that give the book of Qur'an by Shayan Saeed. made by GoodCoding Studios");
+} 
+function where() {
+    alert("We print the book in the printing company. Adress: Al Olaya, Riyadh 11543")
+}
+function close1() {
+    confirm("Sure to close?")
+    close();
+}
+            
+function add() {
+    myWindow = window.open("", "width=200,height=100")
+    myWindow.documnt.write("Work on progress")
+}
+function update() {
+    alert("We have made some updates\nThe book progress is up by 1% from 5% to 6%\nBetter orgaised code to faster load the webpage\nMinor bug fixes\nMuch better looking page\n Improvements on the Donate section");
+}
+function no() {
+    confirm("Our donation system is not working at the momment")
+}
+function show() {
+    var reply;
+    reply = "It is 6% the finished version will be 100%";
+}
+function function0() {
+    str.link("https://www.bankofamerica.com/")
+}
